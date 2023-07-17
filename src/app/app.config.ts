51df0f5a2f32b49f5fc211fa23c6b508f6de5c26
Albert-Chyb <albert7773000@gmail.com>
2023-07-17 23:00:15 +0200

    Initialized the firebase project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,68 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  connectFirestoreEmulator,
+  getFirestore,
+  provideFirestore,
+} from '@angular/fire/firestore';
+import {
+  connectFunctionsEmulator,
+  getFunctions,
+  provideFunctions,
+} from '@angular/fire/functions';
+import {
+  connectStorageEmulator,
+  getStorage,
+  provideStorage,
+} from '@angular/fire/storage';
 import { provideRouter } from '@angular/router';
-
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => {
+        const auth = getAuth();
+
+        if (environment.useFirebaseEmulators) {
+          connectAuthEmulator(auth, 'http://127.0.0.1:9099', {
+            disableWarnings: true,
+          });
+        }
+
+        return auth;
+      }),
+      provideFirestore(() => {
+        const firestore = getFirestore();
+
+        if (environment.useFirebaseEmulators) {
+          connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
+        }
+
+        return firestore;
+      }),
+      provideFunctions(() => {
+        const functions = getFunctions();
+
+        if (environment.useFirebaseEmulators) {
+          connectFunctionsEmulator(functions, '127.0.0.1', 5001);
+        }
+
+        return functions;
+      }),
+      provideStorage(() => {
+        const storage = getStorage();
+
+        if (environment.useFirebaseEmulators) {
+          connectStorageEmulator(storage, '127.0.0.1', 9199);
+        }
+
+        return storage;
+      }),
+    ]),
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'engineering-thesis-fc3ca',
+    appId: '1:930613417880:web:27122ae372598e13393ded',
+    storageBucket: 'engineering-thesis-fc3ca.appspot.com',
+    locationId: 'europe-central2',
+    apiKey: 'AIzaSyCVtEN8UBHdyihCjqUu2gzYe_lrLju_G1g',
+    authDomain: 'engineering-thesis-fc3ca.firebaseapp.com',
+    messagingSenderId: '930613417880',
+  },
+  useFirebaseEmulators: false
+};
