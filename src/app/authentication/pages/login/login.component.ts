b2565f79cbcf3bb98d9ba875c2157d469e669102
Albert Chyb <albert7773000@gmail.com>
2023-08-02 20:32:08 +0200

    Implemented login page

--- src/app/authentication/pages/login/login.component.ts
+++ src/app/authentication/pages/login/login.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoginFormComponent } from '@authentication/components';
+import { AuthService } from '@authentication/services';
+import { LoginFormValue } from '@authentication/types';
+import { Subject, from, map, switchMap } from 'rxjs';
+import { toSignalWithErrors } from 'src/app/common/toSignalWithErrors';
 
 @Component({
   standalone: true,
@@ -8,4 +13,22 @@ import { LoginFormComponent } from '@authentication/components';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  auth = inject(AuthService);
+  router = inject(Router);
+
+  private readonly _loginFormValue = new Subject<LoginFormValue>();
+
+  viewModel = toSignalWithErrors(
+    this._loginFormValue.pipe(
+      switchMap(({ email, password }) => this.auth.signIn(email, password)),
+      switchMap((user) =>
+        from(this.router.navigateByUrl('/')).pipe(map(() => user))
+      )
+    )
+  );
+
+  handleLogin(formValue: LoginFormValue) {
+    this._loginFormValue.next(formValue);
+  }
+}
