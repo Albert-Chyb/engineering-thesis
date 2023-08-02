c061bb5b174759750cb1b20bc575fe10694baec8
Albert Chyb <albert7773000@gmail.com>
2023-08-02 20:16:15 +0200

    Implemented the login form component

--- src/app/authentication/components/login-form/login-form.component.ts
+++ src/app/authentication/components/login-form/login-form.component.ts
@@ -1,11 +1,55 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+import { LoginFormValue } from '../../types/LoginFormValue';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    RouterLink,
+  ],
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
 })
-export class LoginFormComponent {}
+export class LoginFormComponent {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  @Output() login = new EventEmitter<LoginFormValue>();
+
+  handleLoginSubmit() {
+    const { email, password } = this.form.value;
+
+    this.login.emit({
+      email: email as string,
+      password: password as string,
+    });
+  }
+
+  get email() {
+    return this.form.controls.email;
+  }
+
+  get password() {
+    return this.form.controls.password;
+  }
+}
