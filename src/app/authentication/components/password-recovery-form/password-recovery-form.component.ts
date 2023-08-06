def14a37da42bf8cdc023762c4ab9004a564d10d
Albert Chyb <albert7773000@gmail.com>
2023-08-06 19:36:03 +0200

    Created a form for resetting a password

---
+++ src/app/authentication/components/password-recovery-form/password-recovery-form.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { BaseFrom } from 'src/app/common/BaseForm';
+
+interface Form {
+  email: FormControl<string | null>;
+}
+
+@Component({
+  selector: 'app-password-recovery-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatStepperModule,
+  ],
+  templateUrl: './password-recovery-form.component.html',
+  styleUrls: ['./password-recovery-form.component.scss'],
+})
+export class PasswordRecoveryFormComponent extends BaseFrom<Form> {
+  constructor() {
+    const form = new FormGroup<Form>({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+    });
+
+    super(form);
+  }
+
+  get email() {
+    return this.form.controls.email;
+  }
+}
--- src/app/authentication/pages/password-recovery/password-recovery.component.ts
+++ src/app/authentication/pages/password-recovery/password-recovery.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+import { PasswordRecoveryFormComponent } from '../../components/password-recovery-form/password-recovery-form.component';
 
 @Component({
   selector: 'app-password-recovery',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PasswordRecoveryFormComponent],
   templateUrl: './password-recovery.component.html',
-  styleUrls: ['./password-recovery.component.scss']
+  styleUrls: ['./password-recovery.component.scss'],
 })
-export class PasswordRecoveryComponent {
-
-}
+export class PasswordRecoveryComponent {}
