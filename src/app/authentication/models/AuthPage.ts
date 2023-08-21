a83e545ccd913c9eb13de40e66ecfa492004a669
Albert Chyb <albert7773000@gmail.com>
2023-08-21 16:01:48 +0200

    Abstracted the code of the AuthPage class

--- src/app/authentication/models/AuthPage.ts
+++ src/app/authentication/models/AuthPage.ts
@@ -1,53 +1,20 @@
-import {
-  AfterViewInit,
-  Directive,
-  Injector,
-  ViewChild,
-  effect,
-  inject,
-} from '@angular/core';
+import { AfterViewInit, Directive } from '@angular/core';
 import { FirebaseError } from '@angular/fire/app';
 import { AbstractControl } from '@angular/forms';
-import {
-  EMPTY,
-  Observable,
-  Subject,
-  catchError,
-  share,
-  switchMap,
-  throwError,
-} from 'rxjs';
-import { BaseFrom } from 'src/app/common/BaseForm';
+import { EMPTY, Observable, throwError } from 'rxjs';
 import { ConversionMap } from 'src/app/common/ConversionMap';
 import { FirebaseErrorConversionInstruction } from 'src/app/common/FirebaseErrorToFormErrorConversion';
-import { toSignalWithErrors } from 'src/app/common/toSignalWithErrors';
+import { FormActionBasedPage } from 'src/app/common/FormBasedPage';
 
 @Directive()
 export abstract class AuthPage<
-  TForm extends Record<string, AbstractControl<any>>,
-  TFormValue,
-  TTaskResult
-> implements AfterViewInit
+    TForm extends Record<string, AbstractControl<any>>,
+    TFormValue,
+    TTaskResult
+  >
+  extends FormActionBasedPage<TForm, TFormValue, TTaskResult>
+  implements AfterViewInit
 {
-  protected readonly injector = inject(Injector);
-
-  private readonly form$ = new Subject<BaseFrom<TForm, TFormValue>>();
-
-  private readonly formValue$: Observable<TFormValue> = this.form$.pipe(
-    switchMap((formRef) => formRef.onFormValue.asObservable())
-  );
-
-  readonly model$: Observable<TTaskResult> = this.formValue$.pipe(
-    switchMap((formValue) =>
-      this.buildTask(formValue).pipe(
-        catchError((error) => this.handleError(error))
-      )
-    ),
-    share()
-  );
-
-  readonly viewModel = toSignalWithErrors(this.model$);
-
   private readonly errorsConverter = new ConversionMap([
     ['auth/user-not-found', new FirebaseErrorConversionInstruction('email')],
     ['auth/wrong-password', new FirebaseErrorConversionInstruction('password')],
@@ -57,52 +24,16 @@ export abstract class AuthPage<
     ],
   ]);
 
-  @ViewChild(BaseFrom)
-  protected readonly formComponentRef!: BaseFrom<TForm, TFormValue>;
-
-  constructor() {
-    effect(() => {
-      const { data, error } = this.viewModel;
-
-      if (data() && !error()) {
-        this.onSuccessfulTaskCompletion?.();
-      }
-    });
-  }
-
-  ngAfterViewInit(): void {
-    if (!this.formComponentRef) {
-      throw new Error(
-        'An auth page requires a reference to a base form component'
-      );
-    }
-
-    this.form$.next(this.formComponentRef);
-  }
-
-  private handleError(error: unknown): Observable<never> {
+  override handleError(error: unknown): Observable<never> {
     if (
       error instanceof FirebaseError &&
       this.errorsConverter.hasInstruction(error.code)
     ) {
-      this.formComponentRef.errors = this.errorsConverter.convert(
-        error,
-        error.code
-      );
+      this.insertFormError(this.errorsConverter.convert(error, error.code));
 
       return EMPTY;
     } else {
       return throwError(() => error);
     }
   }
-
-  abstract buildTask(formValue: TFormValue): Observable<TTaskResult>;
-}
-
-export interface AuthPage<
-  TForm extends Record<string, AbstractControl<any>>,
-  TFormValue,
-  TTaskResult
-> {
-  onSuccessfulTaskCompletion?(): void;
 }
---
+++ src/app/common/FormBasedPage.ts
@@ -0,0 +1,115 @@
+import { Directive, Injector, ViewChild, effect, inject } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import {
+  Observable,
+  Subject,
+  catchError,
+  share,
+  switchMap,
+  throwError,
+} from 'rxjs';
+import { BaseFrom, FormGroupErrorObject } from './BaseForm';
+import { toSignalWithErrors } from './toSignalWithErrors';
+
+/**
+ * Base class for pages that perform an action based on a form submission.
+ */
+
+@Directive()
+export abstract class FormActionBasedPage<
+  TForm extends Record<string, AbstractControl<any>>,
+  TFormValue,
+  TTaskResult
+> {
+  /**
+   * Reference to the Angular injector.
+   */
+  protected readonly injector = inject(Injector);
+
+  /**
+   * Emits the reference to the form component.
+   */
+  private readonly form$ = new Subject<BaseFrom<TForm, TFormValue>>();
+
+  /**
+   * Emits the form value.
+   */
+  private readonly formValue$: Observable<TFormValue> = this.form$.pipe(
+    switchMap((formRef) => formRef.onFormValue.asObservable())
+  );
+
+  /**
+   * Emits the data returned from the task.
+   */
+  readonly model$: Observable<TTaskResult> = this.formValue$.pipe(
+    switchMap((formValue) =>
+      this.buildTask(formValue).pipe(
+        catchError((error) => this.handleError(error))
+      )
+    ),
+    share()
+  );
+
+  /**
+   * Contains the data returned from the task or the error.
+   */
+  readonly viewModel = toSignalWithErrors(this.model$);
+
+  /**
+   * Reference to the form component.
+   */
+  @ViewChild(BaseFrom)
+  protected readonly formComponentRef!: BaseFrom<TForm, TFormValue>;
+
+  constructor() {
+    effect(() => {
+      const { data, error } = this.viewModel;
+
+      if (data() && !error()) {
+        this.onSuccessfulTaskCompletion?.();
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    if (!this.formComponentRef) {
+      throw new Error(
+        'An auth page requires a reference to a base form component'
+      );
+    }
+
+    this.form$.next(this.formComponentRef);
+  }
+
+  /**
+   * Insert an error in the form.
+   * @param error Error to insert in the form
+   */
+  protected insertFormError(error: FormGroupErrorObject) {
+    this.formComponentRef.errors = error;
+  }
+
+  /**
+   * Build the task that will be executed when the form is submitted.
+   * @param formValue The value of the form
+   */
+  abstract buildTask(formValue: TFormValue): Observable<TTaskResult>;
+
+  /**
+   * Handle an error that occurred during the execution of the task.
+   * This method will be used inside the catchError operator of the model$ observable.
+   * @param error Error to handle
+   * @returns Observable that will be returned by the model$ observable
+   */
+  protected handleError(error: unknown): Observable<never> {
+    return throwError(() => error);
+  }
+}
+
+export interface FormActionBasedPage<
+  TForm extends Record<string, AbstractControl<any>>,
+  TFormValue,
+  TTaskResult
+> {
+  onSuccessfulTaskCompletion?(): void;
+}
