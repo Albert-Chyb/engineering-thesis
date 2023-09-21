d09222f243b6fa816b5aad82ac5316132c202fbf
Albert Chyb <albert7773000@gmail.com>
2023-09-21 16:18:15 +0200

    Created a first draft for the test creator page

--- src/app/test-creator/pages/test-creator-page/test-creator-page.component.ts
+++ src/app/test-creator/pages/test-creator-page/test-creator-page.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatDividerModule],
   templateUrl: './test-creator-page.component.html',
-  styleUrls: ['./test-creator-page.component.scss']
+  styleUrls: ['./test-creator-page.component.scss'],
 })
-export class TestCreatorPageComponent {
-  
-}
+export class TestCreatorPageComponent {}
