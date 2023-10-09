c2493dd609b22a255d113774e5fe0adc536c6b24
Albert Chyb <albert7773000@gmail.com>
2023-10-09 18:40:12 +0200

    Removed the id control from the test form

--- src/app/test-creator/types/test-form-group.ts
+++ src/app/test-creator/types/test-form-group.ts
@@ -2,7 +2,6 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { QuestionFormGroup } from './question-form-group';
 
 export type TestFormGroup = FormGroup<{
-  id: FormControl<string | null>;
   name: FormControl<string | null>;
   questions: FormArray<QuestionFormGroup>;
 }>;
