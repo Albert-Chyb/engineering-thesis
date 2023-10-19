56287f92aeffc6951af91c9522397a37956b50f9
Albert Chyb <albert7773000@gmail.com>
2023-10-19 18:19:22 +0200

    Improved the test form group type

--- src/app/test-creator/types/test-form-group.ts
+++ src/app/test-creator/types/test-form-group.ts
@@ -1,7 +1,8 @@
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { QuestionFormGroup } from './question-form-group';
+import { QuestionsTypes } from './question';
 
 export type TestFormGroup = FormGroup<{
   name: FormControl<string | null>;
-  questions: FormArray<QuestionFormGroup<any>>;
+  questions: FormArray<QuestionFormGroup<QuestionsTypes>>;
 }>;
