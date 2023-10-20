510f06834ececf5b41f2963a95fb1145bfdae57e
Albert Chyb <albert7773000@gmail.com>
2023-10-21 00:36:04 +0200

    Created a directive that helps with utilizing DocumentSync

---
+++ src/app/db-auto-sync/reactive-forms-utils/directives/form-group-sync/form-group-sync.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FormGroupSyncDirective } from './form-group-sync.directive';
+
+describe('FormGroupSyncDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FormGroupSyncDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/db-auto-sync/reactive-forms-utils/directives/form-group-sync/form-group-sync.directive.ts
@@ -0,0 +1,96 @@
+import { Directive, Input, OnInit, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AbstractControl, ControlContainer, FormGroup } from '@angular/forms';
+import { ServerController } from '@db-auto-sync/abstract/server-controller';
+import { DocumentSync } from '@db-auto-sync/core/document-sync';
+import { FormGroupChangeSource } from '@db-auto-sync/reactive-forms-utils/form-group-change-source';
+import {
+  BehaviorSubject,
+  ReplaySubject,
+  Subject,
+  combineLatest,
+  map,
+  switchMap,
+} from 'rxjs';
+
+@Directive({
+  selector: '[appFormGroupSync]',
+  standalone: true,
+})
+export class FormGroupSyncDirective<
+  TControl extends { [K in keyof TControl]: AbstractControl<TControl[K]> }
+> implements OnInit
+{
+  private readonly controlContainer = inject(ControlContainer);
+
+  private readonly _formGroup$ = new ReplaySubject<FormGroup<TControl>>(1);
+  private readonly serverController$ = new Subject<
+    ServerController<FormGroup<TControl>>
+  >();
+  private readonly _docId$ = new ReplaySubject<string>(1);
+  private readonly _excludeFields$ = new BehaviorSubject<(keyof TControl)[]>(
+    []
+  );
+
+  public readonly docSync$ = combineLatest({
+    formGroup: this._formGroup$,
+    serverController: this.serverController$,
+    docId: this._docId$,
+    excludedFields: this._excludeFields$,
+  }).pipe(
+    map(
+      ({ formGroup, serverController, docId, excludedFields }) =>
+        new DocumentSync(
+          new FormGroupChangeSource(
+            formGroup,
+            excludedFields as any // TODO: Improve this type
+          ),
+          serverController,
+          docId
+        )
+    )
+  );
+
+  private readonly sync$ = this.docSync$.pipe(
+    switchMap((docSync) => docSync.init())
+  );
+
+  @Input({ required: true, alias: 'appFormGroupSync' })
+  set serverController(controller: ServerController<FormGroup<TControl>>) {
+    this.serverController$.next(controller);
+  }
+
+  @Input({ required: true }) set docId(id: string) {
+    this._docId$.next(id);
+  }
+
+  @Input() set excludeFields(fields: (keyof TControl)[]) {
+    this._excludeFields$.next(fields);
+  }
+
+  constructor() {
+    this.sync$.pipe(takeUntilDestroyed()).subscribe();
+  }
+
+  ngOnInit(): void {
+    if (this.controlContainer.control instanceof FormGroup === false) {
+      throw new Error(
+        'appFormGroupSync directive can be used only on FormGroup'
+      );
+    }
+
+    this._formGroup$.next(this.controlContainer.control as FormGroup);
+  }
+
+  get formGroup$() {
+    return this._formGroup$.asObservable();
+  }
+
+  get docId$() {
+    return this._docId$.asObservable();
+  }
+
+  get excludeFields$() {
+    return this._excludeFields$.asObservable();
+  }
+}
