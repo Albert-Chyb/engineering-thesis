1bde54ee6f97730bc6340ff05123b70c10074a09
Albert Chyb <albert7773000@gmail.com>
2023-11-08 17:58:30 +0100

    Added a method that swaps positions of two answers

--- src/app/test-creator/services/answers/answers.service.ts
+++ src/app/test-creator/services/answers/answers.service.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import {
   CollectionReference,
   DocumentData,
+  DocumentReference,
   Firestore,
   FirestoreDataConverter,
   PartialWithFieldValue,
@@ -10,13 +11,14 @@ import {
   SnapshotOptions,
   WithFieldValue,
   collection,
+  doc,
   orderBy,
 } from '@angular/fire/firestore';
 import { AuthService } from '@authentication/services/auth.service';
 import { FirestoreCollectionController } from '@common/classes/FirestoreCollectionController';
 import { Answer, RawAnswer } from '@test-creator/types/answer';
 import { ClosedQuestionsTypes } from '@test-creator/types/questions';
-import { Observable, map } from 'rxjs';
+import { Observable, from, map, switchMap, take } from 'rxjs';
 
 class DataConverter
   implements FirestoreDataConverter<Answer<ClosedQuestionsTypes>>
@@ -66,6 +68,42 @@ class AnswersCollectionController extends FirestoreCollectionController<
 
   override list(): Observable<Answer<ClosedQuestionsTypes>[]> {
     return this.query(orderBy('position'));
+  }
+
+  swapPositions(
+    answer1: Answer<ClosedQuestionsTypes>,
+    answer2: Answer<ClosedQuestionsTypes>
+  ) {
+    return this.collectionRef$.pipe(
+      take(1),
+      map((collectionRef) => collectionRef.path),
+      switchMap((collectionPath) =>
+        from(
+          this.transaction(async (transaction) => {
+            const docRef1 = doc(
+              this.firestore,
+              `${collectionPath}/${answer1.id}`
+            ) as DocumentReference<Answer<ClosedQuestionsTypes>>;
+
+            const docRef2 = doc(
+              this.firestore,
+              `${collectionPath}/${answer2.id}`
+            ) as DocumentReference<Answer<ClosedQuestionsTypes>>;
+
+            const doc1 = await transaction.get(docRef1);
+            const doc2 = await transaction.get(docRef2);
+
+            if (!doc1.exists() || !doc2.exists()) {
+              throw new Error('One of the answers does not exist');
+            }
+
+            transaction
+              .update(docRef1, { position: doc2.data().position })
+              .update(docRef2, { position: doc1.data().position });
+          })
+        )
+      )
+    );
   }
 }
 
