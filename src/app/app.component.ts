c4c4e0368b11ab0cdacde8083d53553132847a11
Albert Chyb <albert7773000@gmail.com>
2023-11-13 18:17:53 +0100

    Add createDummyTest function to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '@authentication/services/auth.service';
+import { UserTestsService } from '@test-creator/services/user-tests/user-tests.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,5 +15,23 @@ import { AuthService } from '@authentication/services/auth.service';
 })
 export class AppComponent {
   auth = inject(AuthService);
+  router = inject(Router);
+  userTests = inject(UserTestsService);
   isLoggedIn = toSignal(this.auth.isLoggedIn$);
+
+  createDummyTest() {
+    const id = this.userTests.generateId();
+
+    this.userTests
+      .create(
+        {
+          name: 'Test bez nazwy',
+        },
+        id
+      )
+      .pipe(take(1))
+      .subscribe(() => {
+        this.router.navigateByUrl('/test-creator/' + id);
+      });
+  }
 }
