ef9e96956b994e5c61f13606f5044d809126e0be
Albert Chyb <albert7773000@gmail.com>
2023-11-15 17:10:50 +0100

    Created the component store for user tests

--- src/app/test-creator/pages/user-tests/user-tests.component.ts
+++ src/app/test-creator/pages/user-tests/user-tests.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { UserTestsStore } from './user-tests.store';
 
 @Component({
   standalone: true,
   imports: [CommonModule],
   templateUrl: './user-tests.component.html',
-  styleUrl: './user-tests.component.scss'
+  styleUrl: './user-tests.component.scss',
+  providers: [UserTestsStore],
 })
 export class UserTestsComponent {
-
+  private readonly store = inject(UserTestsStore);
 }
---
+++ src/app/test-creator/pages/user-tests/user-tests.store.ts
@@ -0,0 +1,127 @@
+import { Inject, Injectable, inject } from '@angular/core';
+import { LoadingState } from '@loading-indicator/ngrx/LoadingState';
+import { LoadingStateAdapter } from '@loading-indicator/ngrx/LoadingStateAdapter';
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { UserTestsService } from '@test-creator/services/user-tests/user-tests.service';
+import { Test } from '@test-creator/types/test';
+import { Observable, mergeMap, switchMap, tap } from 'rxjs';
+
+const loadingAdapter = new LoadingStateAdapter();
+
+interface UserTestsState extends LoadingState {
+  tests: Test[];
+  error: any;
+}
+
+const INITIAL_STATE: UserTestsState = {
+  ...loadingAdapter.getInitialState(),
+  tests: [],
+  error: null,
+};
+
+@Injectable()
+export class UserTestsStore extends ComponentStore<UserTestsState> {
+  private readonly userTests = inject(UserTestsService);
+
+  readonly tests = this.selectSignal((state) => state.tests);
+  readonly error = this.selectSignal((state) => state.error);
+  readonly isLoading = this.selectSignal(
+    loadingAdapter.getSelectors().isLoading
+  );
+  readonly isPending = this.selectSignal(
+    loadingAdapter.getSelectors().isPending
+  );
+
+  constructor() {
+    super(INITIAL_STATE);
+  }
+
+  readonly load = this.effect(($) =>
+    $.pipe(
+      tap(() => this.patchState((state) => loadingAdapter.startLoading(state))),
+      switchMap(() => this.userTests.list().pipe()),
+      tapResponse(
+        (tests) =>
+          this.patchState((state) => ({
+            tests,
+            ...loadingAdapter.finishLoading(state),
+          })),
+        (error) =>
+          this.patchState((state) => ({
+            error,
+            ...loadingAdapter.finishLoading(state),
+          }))
+      )
+    )
+  );
+
+  readonly delete = this.effect((testId$: Observable<string>) =>
+    testId$.pipe(
+      tap(() => this.patchState((state) => loadingAdapter.taskStarted(state))),
+      tap((testId) => this._delete(testId)),
+      mergeMap((testId) => {
+        const test = this.get((state) =>
+          state.tests.find((t) => t.id === testId)
+        );
+
+        if (!test) {
+          throw new Error('Test not found');
+        }
+
+        return this.userTests.delete(testId).pipe(
+          tapResponse(
+            () => {
+              this.patchState((state) => ({
+                ...loadingAdapter.taskFinished(state),
+              }));
+            },
+            (error) => {
+              this._add(test);
+
+              this.patchState((state) => ({
+                error,
+                ...loadingAdapter.taskFinished(state),
+              }));
+            }
+          )
+        );
+      })
+    )
+  );
+
+  readonly create = this.effect((test$: Observable<Test>) =>
+    test$.pipe(
+      tap(() => this.patchState((state) => loadingAdapter.taskStarted(state))),
+      tap((test) => this._add(test)),
+      mergeMap((test) =>
+        this.userTests.create(test, test.id).pipe(
+          tapResponse(
+            () => {
+              this.patchState((state) => ({
+                ...loadingAdapter.taskFinished(state),
+              }));
+            },
+            (error) => {
+              this._delete(test.id);
+
+              this.patchState((state) => ({
+                error,
+                ...loadingAdapter.taskFinished(state),
+              }));
+            }
+          )
+        )
+      )
+    )
+  );
+
+  private _delete = this.updater((state, testId: string) => ({
+    ...state,
+    tests: state.tests.filter((test) => test.id !== testId),
+  }));
+
+  private _add = this.updater((state, test: Test) => ({
+    ...state,
+    tests: [...state.tests, test],
+  }));
+}
