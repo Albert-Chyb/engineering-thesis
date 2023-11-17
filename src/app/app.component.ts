a59d139ebfa8312a207f719530851666eeba72b2
Albert Chyb <albert7773000@gmail.com>
2023-11-17 16:11:32 +0100

    Removed createDummyTest function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '@authentication/services/auth.service';
-import { UserTestsService } from '@test-creator/services/user-tests/user-tests.service';
-import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,22 +14,5 @@ import { take } from 'rxjs';
 export class AppComponent {
   auth = inject(AuthService);
   router = inject(Router);
-  userTests = inject(UserTestsService);
   isLoggedIn = toSignal(this.auth.isLoggedIn$);
-
-  createDummyTest() {
-    const id = this.userTests.generateId();
-
-    this.userTests
-      .create(
-        {
-          name: 'Test bez nazwy',
-        },
-        id
-      )
-      .pipe(take(1))
-      .subscribe(() => {
-        this.router.navigateByUrl('/test-creator/' + id);
-      });
-  }
 }
