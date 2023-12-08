01f43686a0b23fdfebb92c24ee6446fa16fe43d8
Albert Chyb <albert7773000@gmail.com>
2023-12-09 00:07:11 +0100

    Handled clicks on actions

--- src/app/tests-sharing/components/shared-tests-actions-bottom-sheet/shared-tests-actions-bottom-sheet.component.ts
+++ src/app/tests-sharing/components/shared-tests-actions-bottom-sheet/shared-tests-actions-bottom-sheet.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+
+export type SharedTestsActions = 'copy-link';
 
 @Component({
   selector: 'app-shared-tests-actions-bottom-sheet',
@@ -11,11 +14,23 @@ import { MatListModule } from '@angular/material/list';
   styleUrl: './shared-tests-actions-bottom-sheet.component.scss',
 })
 export class SharedTestsActionsBottomSheetComponent {
-  readonly actions = [
+  private readonly bottomSheetRef = inject(MatBottomSheetRef);
+
+  readonly actions: {
+    id: SharedTestsActions;
+    title: string;
+    icon: string;
+    description: string;
+  }[] = [
     {
+      id: 'copy-link',
       title: 'Pobierz link',
       icon: 'share',
       description: 'Skopiuj link do schowka',
     },
   ];
+
+  handleActionClick(action: SharedTestsActions) {
+    this.bottomSheetRef.dismiss(action);
+  }
 }
