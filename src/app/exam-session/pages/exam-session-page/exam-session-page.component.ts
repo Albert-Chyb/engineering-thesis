d239b3a85af85111194c18a7aa8e6301a4797f2f
Albert Chyb <albert7773000@gmail.com>
2023-12-18 18:59:25 +0100

    Added a function to load exam session page's store state

--- src/app/exam-session/pages/exam-session-page/exam-session-page.component.ts
+++ src/app/exam-session/pages/exam-session-page/exam-session-page.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ErrorHandler, effect, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PendingIndicatorService } from '@loading-indicator/services/pending-indicator.service';
+import { map } from 'rxjs';
 import { ExamSessionPageStore } from './exam-session-page.store';
 
 @Component({
@@ -9,4 +12,37 @@ import { ExamSessionPageStore } from './exam-session-page.store';
   styleUrl: './exam-session-page.component.scss',
   providers: [ExamSessionPageStore],
 })
-export class ExamSessionPageComponent {}
+export class ExamSessionPageComponent {
+  private readonly store = inject(ExamSessionPageStore);
+  private readonly pendingIndicator = inject(PendingIndicatorService);
+  private readonly route = inject(ActivatedRoute);
+  private readonly errorHandler = inject(ErrorHandler);
+
+  readonly error = this.store.error;
+  readonly metadata = this.store.metadata;
+  readonly test = this.store.test;
+
+  constructor() {
+    this.pendingIndicator.connectStateChanges({
+      onPendingChange$: this.store.pendingIndicatorChanges$,
+    });
+
+    effect(() => {
+      const error = this.error();
+
+      if (error) {
+        this.errorHandler.handleError(error);
+      }
+    });
+
+    this.store.load(this.route.params.pipe(map((params) => {
+      const id = params['id'];
+
+      if (!id) {
+        throw new Error('No test id provided.');
+      }
+
+      return params['id'];
+    })));
+  }
+}
--- src/app/exam-session/pages/exam-session-page/exam-session-page.store.ts
+++ src/app/exam-session/pages/exam-session-page/exam-session-page.store.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { SharedTestsService } from '@exam-session/services/shared-tests.service';
 import { LoadingState } from '@loading-indicator/ngrx/LoadingState';
 import { LoadingStateAdapter } from '@loading-indicator/ngrx/LoadingStateAdapter';
-import { ComponentStore } from '@ngrx/component-store';
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
 import { AssembledTest } from '@test-creator/types/assembled-test';
+import { SharedTestsMetadataService } from '@tests-sharing/services/shared-tests-metadata.service';
 import { SharedTestMetadata } from '@tests-sharing/types/shared-test';
+import { Observable, combineLatest, switchMap, tap } from 'rxjs';
 
 const loadingStateAdapter = new LoadingStateAdapter();
 
@@ -23,7 +26,48 @@ const INITIAL_STATE: ExamSessionPageState = {
 
 @Injectable()
 export class ExamSessionPageStore extends ComponentStore<ExamSessionPageState> {
+  private readonly sharedTests = inject(SharedTestsService);
+  private readonly sharedTestsMetadata = inject(SharedTestsMetadataService);
+
+  readonly pendingIndicatorChanges$ = this.select({
+    isPending: this.select((state) =>
+      loadingStateAdapter.getSelectors().isPending(state.loadingState)
+    ),
+  });
+  readonly error = this.selectSignal((state) => state.error);
+  readonly metadata = this.selectSignal((state) => state.metadata);
+  readonly test = this.selectSignal((state) => state.test);
+
   constructor() {
     super(INITIAL_STATE);
   }
+
+  readonly load = this.effect((id$: Observable<string>) =>
+    id$.pipe(
+      tap(() =>
+        this.patchState((state) => ({
+          loadingState: loadingStateAdapter.startLoading(state.loadingState),
+        }))
+      ),
+      switchMap((id) =>
+        combineLatest({
+          metadata: this.sharedTestsMetadata.getSharedTestMetadata(id),
+          test: this.sharedTests.getSharedTest(id),
+        })
+      ),
+      tapResponse(
+        (data) =>
+          this.patchState((state) => ({
+            metadata: data.metadata,
+            test: data.test,
+            loadingState: loadingStateAdapter.finishLoading(state.loadingState),
+          })),
+        (error) =>
+          this.patchState((state) => ({
+            error,
+            loadingState: loadingStateAdapter.finishLoading(state.loadingState),
+          }))
+      )
+    )
+  );
 }
