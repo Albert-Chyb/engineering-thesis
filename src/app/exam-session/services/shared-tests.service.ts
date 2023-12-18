f1bdb337d463932ca8d603c51f44d69853eaba0a
Albert Chyb <albert7773000@gmail.com>
2023-12-18 18:56:29 +0100

    Created a service for the shared tests collection

---
+++ src/app/exam-session/services/shared-tests.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import {
+  CollectionReference,
+  DocumentData,
+  FirestoreDataConverter,
+  PartialWithFieldValue,
+  QueryDocumentSnapshot,
+  SetOptions,
+  SnapshotOptions,
+  WithFieldValue,
+  collection,
+  doc,
+  getDoc,
+  getFirestore,
+} from '@angular/fire/firestore';
+import {
+  AssembledTest,
+  AssembledTestSchema,
+} from '@test-creator/types/assembled-test';
+import { Observable, from, map } from 'rxjs';
+
+class DataConverter implements FirestoreDataConverter<AssembledTest> {
+  toFirestore(modelObject: WithFieldValue<AssembledTest>): DocumentData;
+  toFirestore(
+    modelObject: PartialWithFieldValue<AssembledTest>,
+    options: SetOptions
+  ): DocumentData;
+  toFirestore(modelObject: unknown, options?: unknown): DocumentData {
+    throw new Error('Direct modification of shared tests is not allowed.');
+  }
+
+  fromFirestore(
+    snapshot: QueryDocumentSnapshot<DocumentData>,
+    options?: SnapshotOptions | undefined
+  ): AssembledTest {
+    return AssembledTestSchema.parse({
+      ...snapshot.data(options),
+      id: snapshot.id,
+    });
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SharedTestsService {
+  private readonly firestore = getFirestore();
+
+  getSharedTest(id: string): Observable<AssembledTest> {
+    return from(getDoc(doc(this.getSharedTestCollectionRef(), id))).pipe(
+      map((snap) => {
+        const testData = snap.data();
+
+        if (!testData) {
+          throw new Error('Shared test not found.');
+        }
+
+        return testData;
+      })
+    );
+  }
+
+  private getSharedTestCollectionRef(): CollectionReference<AssembledTest> {
+    return collection(this.firestore, 'shared-tests').withConverter(
+      new DataConverter()
+    );
+  }
+}
