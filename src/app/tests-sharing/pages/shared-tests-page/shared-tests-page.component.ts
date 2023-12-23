03ee30afbda3c5ddd8fbb324d6ea887d626b7700
Albert-Chyb <albert7773000@gmail.com>
2023-12-23 18:42:53 +0100

    Refactored the shared tests page to use bottom sheet actions utility

--- src/app/tests-sharing/pages/shared-tests-page/shared-tests-page.component.ts
+++ src/app/tests-sharing/pages/shared-tests-page/shared-tests-page.component.ts
@@ -1,22 +1,16 @@
 import { Clipboard, ClipboardModule } from '@angular/cdk/clipboard';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import {
-  MatBottomSheet,
-  MatBottomSheetModule,
-} from '@angular/material/bottom-sheet';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { LoadingIndicatorComponent } from '@loading-indicator/components/loading-indicator/loading-indicator.component';
 import { PendingIndicatorService } from '@loading-indicator/services/pending-indicator.service';
-import {
-  SharedTestsActions,
-  SharedTestsActionsBottomSheetComponent,
-} from '@tests-sharing/components/shared-tests-actions-bottom-sheet/shared-tests-actions-bottom-sheet.component';
 import { SharedTestsMetadataService } from '@tests-sharing/services/shared-tests-metadata.service';
-import { take } from 'rxjs';
+import { BottomSheetAction } from '@utils/bottom-sheet-actions/bottom-sheet-action';
+import { BottomSheetActionsTriggerDirective } from '@utils/bottom-sheet-actions/bottom-sheet-actions-trigger.directive';
 import { SharedTestsPageStore } from './shared-tests-page.store';
 
 @Component({
@@ -30,6 +24,7 @@ import { SharedTestsPageStore } from './shared-tests-page.store';
     MatBottomSheetModule,
     ClipboardModule,
     MatSnackBarModule,
+    BottomSheetActionsTriggerDirective,
   ],
   templateUrl: './shared-tests-page.component.html',
   styleUrl: './shared-tests-page.component.scss',
@@ -38,7 +33,6 @@ import { SharedTestsPageStore } from './shared-tests-page.store';
 export class SharedTestsPageComponent {
   private readonly store = inject(SharedTestsPageStore);
   private readonly pendingIndicator = inject(PendingIndicatorService);
-  private readonly bottomSheets = inject(MatBottomSheet);
   private readonly clipboard = inject(Clipboard);
   private readonly snackBars = inject(MatSnackBar);
   private readonly sharedTests = inject(SharedTestsMetadataService);
@@ -55,30 +49,34 @@ export class SharedTestsPageComponent {
     this.store.load();
   }
 
-  showTestsActions(id: string) {
-    const bottomSheetRef = this.bottomSheets.open<
-      SharedTestsActionsBottomSheetComponent,
-      void,
-      SharedTestsActions
-    >(SharedTestsActionsBottomSheetComponent);
+  readonly pageActions: BottomSheetAction[] = [
+    {
+      name: 'copy-link',
+      title: 'Pobierz link',
+      icon: 'share',
+      description: 'Skopiuj link do schowka',
+    },
+    {
+      name: 'show-solved-tests',
+      title: 'Rozwiązane testy',
+      icon: 'pageview',
+      description: 'Przeglądaj przesłane rozwiązania',
+    },
+  ];
 
-    bottomSheetRef
-      .afterDismissed()
-      .pipe(take(1))
-      .subscribe((action) => {
-        switch (action) {
-          case 'copy-link':
-            const link = this.sharedTests.generateLink(id);
+  handlePageAction(actionName: string, id: string) {
+    switch (actionName) {
+      case 'copy-link':
+        const link = this.sharedTests.generateLink(id);
 
-            this.copyToClipboard(link);
-            this.snackBars.open('Skopiowano link do schowka', 'Zamknij');
-            break;
+        this.copyToClipboard(link);
+        this.snackBars.open('Skopiowano link do schowka', 'Zamknij');
+        break;
 
-          case 'show-solved-tests':
-            this.router.navigate(['shared-tests', id, 'submitted-solutions']);
-            break;
-        }
-      });
+      case 'show-solved-tests':
+        this.router.navigate(['shared-tests', id, 'submitted-solutions']);
+        break;
+    }
   }
 
   private copyToClipboard(text: string): void {
