05e47d2e9f53808b8fa081f8199192f2b8cdcb21
Albert-Chyb <albert7773000@gmail.com>
2023-12-24 14:21:40 +0100

    Refactor loading-indicator to not use mat card module

--- src/app/loading-indicator/components/loading-indicator/loading-indicator.component.ts
+++ src/app/loading-indicator/components/loading-indicator/loading-indicator.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-loading-indicator',
   standalone: true,
-  imports: [CommonModule, MatProgressSpinnerModule, MatCardModule],
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './loading-indicator.component.html',
   styleUrl: './loading-indicator.component.scss',
 })
