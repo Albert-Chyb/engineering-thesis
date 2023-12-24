56fe446204041cab19a5747f0936c425de7861b2
Albert-Chyb <albert7773000@gmail.com>
2023-12-24 18:19:12 +0100

    Created a store method that loads the page data

--- src/app/tests-grading/pages/test-grading-page/test-grading-page.component.ts
+++ src/app/tests-grading/pages/test-grading-page/test-grading-page.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, map } from 'rxjs';
 import { TestGradingPageStore } from './test-grading-page.store';
 
 @Component({
@@ -11,4 +13,30 @@ import { TestGradingPageStore } from './test-grading-page.store';
 })
 export class TestGradingPageComponent {
   private readonly store = inject(TestGradingPageStore);
+  private readonly route = inject(ActivatedRoute);
+
+  readonly solvedTest = this.store.solvedTest;
+  readonly solvedTestAnswers = this.store.solvedTestAnswers;
+
+  constructor() {
+    this.store.load(this.getParams());
+  }
+
+  private getParams(): Observable<{
+    sharedTestId: string;
+    solvedTestId: string;
+  }> {
+    return this.route.paramMap.pipe(
+      map((params) => {
+        const sharedTestId = params.get('sharedTestId');
+        const solvedTestId = params.get('solvedTestId');
+
+        if (!sharedTestId || !solvedTestId) {
+          throw new Error('Missing params');
+        }
+
+        return { sharedTestId, solvedTestId };
+      }),
+    );
+  }
 }
--- src/app/tests-grading/pages/test-grading-page/test-grading-page.store.ts
+++ src/app/tests-grading/pages/test-grading-page/test-grading-page.store.ts
@@ -1,21 +1,74 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { SolvedTestsService } from '@exam-session/services/solved-tests.service';
 import { LoadingState } from '@loading-indicator/ngrx/LoadingState';
 import { LoadingStateAdapter } from '@loading-indicator/ngrx/LoadingStateAdapter';
-import { ComponentStore } from '@ngrx/component-store';
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { SolvedTestsAnswersService } from '@tests-grading/services/solved-tests-answers.service';
+import { SolvedTest } from '@tests-grading/types/solved-test';
+import { SolvedTestAnswers } from '@tests-grading/types/solved-test-answers';
+import { Observable, combineLatest, switchMap, tap } from 'rxjs';
 
 const loadingAdapter = new LoadingStateAdapter();
 
 interface TestGradingPageState {
   loadingState: LoadingState;
+  error: unknown;
+  solvedTest: SolvedTest | null;
+  solvedTestAnswers: SolvedTestAnswers | null;
 }
 
 const INITIAL_STATE: TestGradingPageState = {
   loadingState: loadingAdapter.getInitialState(),
+  error: null,
+  solvedTestAnswers: null,
+  solvedTest: null,
 };
 
 @Injectable()
 export class TestGradingPageStore extends ComponentStore<TestGradingPageState> {
+  private readonly solvedTestsAnswers = inject(SolvedTestsAnswersService);
+  private readonly solvedTests = inject(SolvedTestsService);
+
+  readonly solvedTest = this.selectSignal((state) => state.solvedTest);
+  readonly solvedTestAnswers = this.selectSignal(
+    (state) => state.solvedTestAnswers,
+  );
+
   constructor() {
     super(INITIAL_STATE);
   }
+
+  readonly load = this.effect(
+    (ids$: Observable<{ sharedTestId: string; solvedTestId: string }>) =>
+      ids$.pipe(
+        tap(() =>
+          this.patchState((state) => ({
+            loadingState: loadingAdapter.startLoading(state.loadingState),
+          })),
+        ),
+        switchMap(({ sharedTestId, solvedTestId }) =>
+          combineLatest({
+            solvedTest: this.solvedTests.get(sharedTestId, solvedTestId),
+            solvedTestAnswers: this.solvedTestsAnswers.get(
+              sharedTestId,
+              solvedTestId,
+            ),
+          }),
+        ),
+        tapResponse(
+          ({ solvedTestAnswers, solvedTest }) =>
+            this.patchState((state) => ({
+              solvedTestAnswers,
+              solvedTest,
+              loadingState: loadingAdapter.finishLoading(state.loadingState),
+            })),
+          (error) => {
+            return this.patchState((state) => ({
+              error,
+              loadingState: loadingAdapter.finishLoading(state.loadingState),
+            }));
+          },
+        ),
+      ),
+  );
 }
