7810198c67adc6b8e27586695ec13c38db166bb9
Albert-Chyb <albert7773000@gmail.com>
2023-12-25 23:22:48 +0100

    Exposed the record value type

--- src/app/tests-grading/types/solved-test-answers.ts
+++ src/app/tests-grading/types/solved-test-answers.ts
@@ -2,7 +2,7 @@ import { QuestionsAnswerSchema } from '@exam-session/types/user-answers';
 import { QuestionsTypesEnum } from '@test-creator/types/question';
 import { z } from 'zod';
 
-const SolvedTestAnswerRecordValueSchema = z.object({
+export const SolvedTestAnswerRecordValueSchema = z.object({
   isCorrect: z.boolean().nullable(),
   answer: QuestionsAnswerSchema.valueSchema,
   questionType: QuestionsTypesEnum,
@@ -26,3 +26,6 @@ export const SolvedTestAnswersSchema = z.object({
 
 export type SolvedTestAnswers = z.infer<typeof SolvedTestAnswersSchema>;
 export type SolvedTestAnswer = z.infer<typeof SolvedTestAnswerSchema>;
+export type SolvedTestAnswerRecordValue = z.infer<
+  typeof SolvedTestAnswerRecordValueSchema
+>;
