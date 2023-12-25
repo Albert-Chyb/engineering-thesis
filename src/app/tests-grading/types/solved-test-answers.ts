2a84828337a2b0cdf8eef603d46f6a252fc8127e
Albert-Chyb <albert7773000@gmail.com>
2023-12-25 17:41:07 +0100

    Added transformation of the answers field into an array

--- src/app/tests-grading/types/solved-test-answers.ts
+++ src/app/tests-grading/types/solved-test-answers.ts
@@ -2,22 +2,21 @@ import { QuestionsAnswerSchema } from '@exam-session/types/user-answers';
 import { QuestionsTypesEnum } from '@test-creator/types/question';
 import { z } from 'zod';
 
+export const SolvedTestAnswerSchema = z.object({
+  isCorrect: z.boolean().nullable(),
+  answer: QuestionsAnswerSchema.valueSchema,
+  questionType: QuestionsTypesEnum,
+});
+
 export const SolvedTestAnswersSchema = z.object({
   id: z.string(),
-  answers: z
-    .record(
-      z.object({
-        isCorrect: z.boolean().nullable(),
-        answer: QuestionsAnswerSchema.valueSchema,
-        questionType: QuestionsTypesEnum,
-      }),
-    )
-    .transform((record) =>
-      Object.entries(record).map(([key, value]) => ({
-        questionId: key,
-        ...value,
-      })),
-    ),
+  answers: z.record(SolvedTestAnswerSchema).transform((record) =>
+    Object.entries(record).map(([key, value]) => ({
+      questionId: key,
+      ...value,
+    })),
+  ),
 });
 
 export type SolvedTestAnswers = z.infer<typeof SolvedTestAnswersSchema>;
+export type SolvedTestAnswer = z.infer<typeof SolvedTestAnswerSchema>;
