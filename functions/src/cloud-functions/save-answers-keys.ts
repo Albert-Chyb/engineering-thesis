a08c7bfadbae694b8c35334f792ade272ca040c7
Albert-Chyb <albert7773000@gmail.com>
2023-12-26 22:47:13 +0100

    Created a cloud function that saves answers keys to database

---
+++ functions/src/cloud-functions/save-answers-keys.ts
@@ -0,0 +1,53 @@
+import { HttpsError, onCall } from 'firebase-functions/v2/https';
+import { z } from 'zod';
+import { answersKeysDoc } from '../data-access/answers-keys';
+import { ensureExistence } from '../data-access/common';
+import { sharedTest } from '../data-access/shared-tests';
+import { answersKeysSchema } from '../models/answers-keys';
+import { solvedTestAnswerSchema } from '../models/solved-test-answers';
+
+export const saveAnswersKeysDataSchema = z.object({
+  answersKeys: z.record(solvedTestAnswerSchema.shape.answer),
+  sharedTestId: z.string(),
+});
+
+export const saveAnswersKeys = onCall<
+  z.infer<typeof saveAnswersKeysDataSchema>
+>(async (req) => {
+  const userId = req.auth?.uid;
+
+  if (!userId) {
+    throw new HttpsError('unauthenticated', 'User is not authenticated');
+  }
+
+  const dataVerification = saveAnswersKeysDataSchema.safeParse(req.data);
+
+  if (!dataVerification.success) {
+    throw new HttpsError(
+      'invalid-argument',
+      'Invalid data passed to the function',
+      dataVerification.error.issues.map((issue) => issue.message),
+    );
+  }
+
+  const { answersKeys, sharedTestId } = dataVerification.data;
+  const sharedTestSnap = await ensureExistence(sharedTest(sharedTestId));
+  const answersKeysRef = answersKeysDoc(sharedTestId);
+
+  const answersKeysDocVerification = answersKeysSchema.safeParse({
+    answersKeys,
+    sharedTest: sharedTestSnap.data(),
+  });
+
+  if (!answersKeysDocVerification.success) {
+    throw new HttpsError(
+      'internal',
+      'Could not save answers keys',
+      answersKeysDocVerification.error.issues
+        .map((issue) => issue.message)
+        .join('\n'),
+    );
+  }
+
+  await answersKeysRef.set(answersKeysDocVerification.data);
+});
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,5 +5,6 @@ initializeApp({
 });
 
 export { evaluateSolvedTestAnswers } from './cloud-functions/evaluate-solved-test-answers';
+export { saveAnswersKeys } from './cloud-functions/save-answers-keys';
 export { saveSolvedTest } from './cloud-functions/save-solved-test';
 export { shareTest } from './cloud-functions/share-test';
