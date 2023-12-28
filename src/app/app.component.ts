eedd5c6686ad94038ecb46a5486e81f945489b0c
Albert-Chyb <albert7773000@gmail.com>
2023-12-28 18:59:56 +0100

    Created a sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,11 @@ import { AfterViewInit, Component, ViewChild, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { AuthService } from '@authentication/services/auth.service';
 import { PendingIndicatorComponent } from '@loading-indicator/components/pending-indicator/pending-indicator.component';
 import { PendingIndicatorService } from '@loading-indicator/services/pending-indicator.service';
@@ -17,13 +19,14 @@ import { map } from 'rxjs';
   standalone: true,
   imports: [
     CommonModule,
-    RouterOutlet,
-    RouterLink,
+    RouterModule,
     PendingIndicatorComponent,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatTooltipModule,
+    MatSidenavModule,
+    MatListModule,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
@@ -42,6 +45,21 @@ export class AppComponent implements AfterViewInit {
       .observe(Breakpoints.XSmall)
       .pipe(map((result) => result.matches)),
   );
+
+  readonly mainNavRoutes = [
+    {
+      link: '/my-tests',
+      icon: 'edit_note',
+      title: 'Utworzone testy',
+      description: 'Przeglądaj wersje robocze testów',
+    },
+    {
+      link: '/shared-tests',
+      icon: 'shared-tests',
+      title: 'Udostępnione testy',
+      description: 'Przeglądaj udostępnione publicznie testy',
+    },
+  ];
 
   ngAfterViewInit(): void {
     this.pendingIndicator.connectIndicator(this.pendingIndicatorComponent);
