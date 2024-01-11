5cbb4b347ebeaf5b91d6a53523ba30ccccc8e210
Albert-Chyb <albert7773000@gmail.com>
2024-01-11 18:29:01 +0100

    Added testTakerId field

--- src/app/tests-grading/types/solved-test.ts
+++ src/app/tests-grading/types/solved-test.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 
 export const SolvedTestSchema = z.object({
   testTakerName: z.string(),
+  testTakerId: z.string(),
   sharedTestId: z.string(),
   id: z.string(),
   date: z.instanceof(Timestamp).transform((timestamp) => timestamp.toDate()),
