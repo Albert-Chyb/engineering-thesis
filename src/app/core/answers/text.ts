4fa6eed8869a8d92715a6bb7639075c79033d982
Albert-Chyb <albert7773000@gmail.com>
2024-02-01 23:01:31 +0100

    Delete unused code

--- src/app/core/answers/text.ts
+++
@@ -1,9 +0,0 @@
-import { Answer } from '../abstract/answer';
-
-export class TextAnswer extends Answer<'text-answer'> {
-  override copy(): TextAnswer {
-    const copy = new TextAnswer(this.id, this.content);
-
-    return copy;
-  }
-}
